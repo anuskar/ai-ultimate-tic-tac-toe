bb9a02da4e3dfb1367317eed1dc913f0ddabd9c5
NoahNU <noahnu@gmail.com>
2017-08-13 16:19:53 -0400

    added baseline, formatting

--- compare_heuristics.py
+++ compare_heuristics.py
@@ -25,16 +25,12 @@ def trace(*args):
     if TRACE:
         print(*args)
 
-"""
-Entry point for Ultimate Tic Tac Toe game tree demo.
-"""
-def main():
+def against_random(trials, depths, heuristics):
     """
-    Collects win and runtime stats on each heuristic at differing depths.
+    Tests each heuristic against a random player.
     """
-    trials = 5
-    depth = [1, 2, 3]
-    heuristics = { 'A': heuristic_A, 'B': heuristic_B }
+
+    print("=== Heuristic against Random ===")
 
     # Testing for standard UTTT board.
     print("{:<12}{:<15}{:<8}{:<15}{:<13}{:<15}".format("# Trials", "Heuristic", "Depth", "Avg Runtime", "Win Rate", "Player 0"))
@@ -45,7 +41,7 @@ def main():
         all_time = 0
         all_wins = 0
 
-        for d in depth:
+        for d in depths:
             trace("[Heuristic {}; Depth: {}]".format(hName, d))
 
             all_order_time = 0
@@ -96,10 +92,87 @@ def main():
             all_wins += percent_win
 
         # Average over opponent starting vs. AI starting.
-        avg_time = all_time / float(len(depth) + 1)
-        percent_win = all_wins / float(len(depth) + 1)
+        avg_time = all_time / float(len(depths) + 1)
+        percent_win = all_wins / float(len(depths) + 1)
 
-        print("{:<12}{:<15}{:<8}{:<15.3f}{:<13.3f}{:<15}".format(trials * 2 * (range(depth) + 1), hName, 'All', avg_time, percent_win, 'Both'))
+        print("{:<12}{:<15}{:<8}{:<15.3f}{:<13.3f}{:<15}".format(trials * 2 * (range(depths) + 1), hName, 'All', avg_time, percent_win, 'Both'))
+
+
+def baseline():
+    """
+    Pits random against random.
+    """
+
+    print("== Random against Random ==")
+
+    # Testing for standard UTTT board.
+    print("{:<12}{:<15}{:<13}{:<15}".format("# Trials", "Avg Runtime", "Win Rate", "Player 0"))
+
+    trials = 50
+    all_time = 0
+    all_wins = 0
+
+    for opponent_first in [True, False]:
+        player_zero = ("Opponent" if opponent_first else "AI")
+
+        trace("Player Zero: {}]".format(player_zero))
+
+        # Force AI to be Player with id 0 always.
+        initial_state = UTTTState(1 if opponent_first else 0, None, None, UTTTSpace(None, 3, 3))
+
+        i_time = 0
+        i_wins = 0
+
+        for t in range(trials):
+            trace("[Player Zero: {}; Trial: {}]".format(player_zero, t))
+
+            # Instantiate the game tree.
+            game = GameTree(initial_state, opponent_first)
+
+            # Test against random.
+            game.attach_turn_handler(always_choose_random_state)
+            game.attach_tester_handler(always_choose_random_state)
+
+            game.DICTATE = DICTATE
+
+            start = time.time()
+            win = game.play()
+            i_time += time.time() - start
+            i_wins += 1 if win else 0
+
+        avg_time = i_time / trials
+        percent_win = i_wins / trials
+
+        print("{:<12}{:<15.3f}{:<13.3f}{:<15}".format(trials, avg_time, percent_win, player_zero))
+
+        all_time += avg_time
+        all_wins += percent_win
+
+    # Average over opponent starting vs. AI starting.
+    avg_time = all_time / 2.0
+    percent_win = all_wins / 2.0
+
+    print("{:<12}{:<15.3f}{:<13.3f}{:<15}".format(trials * 2, avg_time, percent_win, 'Both'))
+
+"""
+Entry point for Ultimate Tic Tac Toe game tree demo.
+"""
+def main():
+    """
+    Collects win and runtime stats on each heuristic at differing depths.
+    """
+    trials = 5
+    depths = [1, 2, 3]
+    heuristics = { 'A': heuristic_A, 'B': heuristic_B }
+
+    print("===")
+    print("Performing tests...")
+    print("===")
+    print("")
+
+    baseline()
+    print("")
+    against_random(trials, depths, heuristics)
 
 
 if __name__ == "__main__":
